c4c607ac9416496c36e658aa65366a265de173b7
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-03-10 22:48:02 -0400

    Task 3 - Removing Risk Items

--- cc_15.js
+++ cc_15.js
@@ -3,6 +3,7 @@
 const riskDash = document.getElementById('riskDashboard') // select risk dashboard
 console.log("Risk Dashboard Loaded") // print "Risk Dashboard Loaded" to the console
 
+// Task 2
 function addRiskItem(riskName, riskLevel, department){
     const riskCard = document.createElement('div')
     riskCard.setAttribute('class',"riskCard") // sets riskcard class
@@ -14,6 +15,15 @@ function addRiskItem(riskName, riskLevel, department){
 `;
     riskDash.appendChild(riskCard) // appends riskCard to the dashboard
 
+//Task 3
+    const resolveButton = document.createElement("button") // creates button element
+    resolveButton.setAttribute('class','resolveButton') // sets button class
+    resolveButton.textContent = 'Resolve' // adds text to button
+    
+    resolveButton.addEventListener('click',()=>{ // removes card from dash when clicked
+        riskDash.removeChild(riskCard)
+    })
+    riskCard.appendChild(resolveButton) // appends resolve button to card
 }
 
 const riskForm = document.getElementById("riskForm") // assigns value to a constant
@@ -28,3 +38,7 @@ riskForm.addEventListener('submit', () =>{ // creates an event listener that lis
     event.target.reset(); // Reset the form
 })
 
+//Test cases
+addRiskItem("Market Fluctuations", "High", "Finance");
+addRiskItem("Data Breach", "High", "IT");
+addRiskItem("Supply Chain Disruption", "Medium", "Operations");
